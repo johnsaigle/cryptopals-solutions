cdd41277bdf2c2031f06281f0c92fe43e566c56d
John Saigle <nothanks>
2024-02-18 14:32:13 -0500

    rewrite challs 1 and 2

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,66 @@
-use std::io;
-use std::fs;
-use hex::decode;
-use base64::{encode};
-use std::ops::BitXor;
-use anyhow::{Result,Error};
-use std::collections::HashMap;
-use std::collections::BTreeMap;
+// Using unit tests which mean functions are never called in a normal way
+#![allow(dead_code)]
 
-fn hex_to_base64(buffer: String) -> Result<String, anyhow::Error> {
-    let hex = hex::decode(buffer)?;
-    Ok(base64::encode(hex))
+use::core::iter::zip;
+
+
+fn hex_to_base64(input: &str) -> String {
+    base64::encode(hex::decode(input).unwrap())
 }
 
-fn challenge2() -> Result<Vec<u8>, anyhow::Error> {
-    let v1 = hex::decode("1c0111001f010100061a024b53535009181c")?;
-    let v2 = hex::decode("686974207468652062756c6c277320657965")?;
-    let v3: Vec<u8> = v1
-        .iter()
-        .zip(v2.iter())
-        .map(|(&x1, &x2)| x1 ^ x2)
+fn xor(left: Vec<u8>, right: Vec<u8>) -> Vec<u8> {
+     let result: Vec<u8> = 
+        zip(left, right)
+        .into_iter()
+        .map(|(l, r)| l ^ r)
         .collect();
-    Ok(v3)
+
+    return result;
 }
 
-fn challenge3() -> Result<u8, anyhow::Error> {
-    let cipher = hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
-    // change this to loop 0-255
-    const alphabet: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+fn freq_analysis(ciphertext: Vec<u8>) -> {
+    // Create a normal distribution of characters in English
+    // Take input string
+    // XOR against a character (I guess try all 255 ASCII)
+    // Count occurrences of all characters in the result
+    // Compare character count against normal distribution
+    // Use e.g. Hamming distance to score the similarity
+    // The answer is the XOR character with the resulting frequency closest
+    //  to the normal distribution
     
-    // score
-    // compare frequencies of letters in decryption to known frequency of english characters
-    // build dict from sample
-    // store it in a vector
-    // do euclidean distance between vectors; find smallest
-    let contents = fs::read_to_string("sample-lower.txt")
-        .expect("Should have been able to read the file");
+    // Loop over all possible keys
+    for i in 0..u8::MAX {
 
-    let mut letters = HashMap::new();
-
-    for ch in contents.chars().filter(|x| x.is_alphabetic() && x.is_ascii()) {
-        letters.entry(ch).and_modify(|counter| *counter += 1).or_insert(1);
     }
-    let count_b: BTreeMap<&u32,&char> = letters.iter().map(|(k,v)| (v,k)).collect();
-    println!("{:?}", count_b);
-    //score -- do Euclidean distance on the vectors
-    Ok(1)
+
 }
 
-fn main() -> Result<()> {
-    let mut buffer = String::new();
-    let stdin = io::stdin(); // We get `Stdin` here.
-    //stdin.read_line(&mut buffer)?;
+fn main() {
 
-    // Hex to base64
-    //let res = hex_to_base64(buffer)?;
-    
-    //
-    let res = challenge2()?;
-    println!("{:?}", res);
-    println!("{:?}", hex::encode(res));
-    challenge3();
-    Ok(())
+}
+
+
+#[test]
+fn test_set1_challenge1() {
+    let c1_input: String = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
+    assert_eq!("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", hex_to_base64(&c1_input));
+}
+
+#[test]
+fn test_set1_challenge2() {
+    let slice_a: String = "1c0111001f010100061a024b53535009181c".to_string();
+    let slice_b: String = "686974207468652062756c6c277320657965".to_string();
+    assert_eq!(
+        "746865206b696420646f6e277420706c6179".to_string(),
+        hex::encode(
+        xor(
+        hex::decode(slice_a).unwrap(),
+        hex::decode(slice_b).unwrap()
+        ))
+    );
+}
+
+#[test]
+fn test_set1_challenge3() {
+    let input: String = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".to_string();
 }
