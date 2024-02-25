9fa0bc644ecff17654dc5840ef41e26e7b1c5f8b
John Saigle <nothanks>
2024-02-25 09:27:25 -0500

    set 1 challenge 5 solution

--- src/main.rs
+++ src/main.rs
@@ -130,6 +130,18 @@ fn detect_single_byte_xor() -> Result<(Vec<u8>, u32), anyhow::Error> {
     Ok((plaintext, smallest_distance))
 }
 
+fn encrypt_repeating_key_xor(plaintext: Vec<u8>, key: Vec<u8>) -> Vec<u8> {
+
+    // let key = "ICE".as_bytes();
+    // Do division: q = mn + r. 
+    let quotient = plaintext.len() / key.len();
+    let remainder = plaintext.len() % key.len();
+    let mut key_vector: Vec<u8> = key.repeat(quotient);
+    key_vector.extend_from_slice(&key[0..remainder]);
+
+    xor(plaintext, key_vector)
+}
+
 fn main() {}
 
 #[test]
@@ -177,5 +189,19 @@ fn test_set1_challenge4() {
     assert_eq!(
         "Now that the party is jumping\n".to_string(),
         plaintext_string
+    );
+}
+
+#[test]
+fn test_set1_challenge5() {
+    let plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes().to_vec();
+    let key = "ICE".as_bytes().to_vec();
+
+    let ciphertext = encrypt_repeating_key_xor(plaintext, key);
+    let ciphertext_hexstring = hex::encode(ciphertext);
+
+    assert_eq!(
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
+        ciphertext_hexstring
     );
 }
